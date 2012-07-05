53ecb149bc2f289033852870ab9a429023cd6221
melshochet <melshochet@a07cae6d-afd3-2c87-918b-c8c103bdeca4>
2012-07-05 15:09:48 +0000

    Add svn editor

--- TriggerValidation/trunk/setup_tier3.sh
+++ TriggerValidation/trunk/setup_tier3.sh
@@ -6,6 +6,7 @@
 #echo "Setting up voms-proxy. Please enter your grid password:"
 #source /share/wlcg-client/setup.sh && voms-proxy-init -voms atlas -valid 999:0
 
+export SVN_EDITOR="emacs"
 atlasrepo=/cvmfs/atlas.cern.ch/repo/sw/software/x86_64-slc5-gcc43-opt/
 export AtlasSetup=${atlasrepo}/17.1.4/AtlasSetup
 echo "Please wait 20-30 seconds: setting up the tier3 computing environment"
