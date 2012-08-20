6540a55cf2feef275bf8e4baeab2337a4de8517b
yangyangchng <yangyangchng@a07cae6d-afd3-2c87-918b-c8c103bdeca4>
2012-08-20 18:30:09 +0000

    update

--- TriggerValidation/trunk/Bug.py
+++ TriggerValidation/trunk/Bug.py
@@ -249,7 +249,7 @@ class BugTracker:
             s.add(96602,["AllPartition_physicsV4","nightlies/17.1.X/","test killed as time quota spent, test warning is issued"],cat=2)
             s.add(96635,["AllPT_HIV2_run_stop_run","nightlies/17.1.X.Y-VAL2-P1HLT/","test killed as time quota spent, test warning is issued"],cat=2)
             s.add(96714,["AthenaTrigRDO_MC_pp_v4","mc_prescale","nightlies/17.1.X.Y.Z-VAL-AtlasCAFHLT/","test killed as time quota spent, test warning is issued"],cat=2)
-            #s.add(     ,["AthenaTrigRDO_MC_pp_v4_tight_mc_prescale","nightlies/17.1.X.Y.Z-VAL-AtlasCAFHLT/","test killed as time quota spent, test warning is issued"],cat=2)
+            s.add(96882,["AthenaTrigRDO_MC_pp_v4_loose_mc_prescale","nightlies/17.1.X.Y.Z-VAL2-AtlasCAFHLT/","test killed as time quota spent, test warning is issued"],cat=2)
             # TOLERANCE BUGS
             s.add(-8,"checkcounts test warning : trigger counts outside tolerance:",title="ATTENTION: add a TOLERANCE BUG match string in Bug.py::prefill_nicos for this release",cat=1) #HAS to be above other tolerance  strings - this is a "catch-all" case
             s.add(-96368,["checkcounts test warning : trigger counts outside tolerance:","nightlies/17.1.X.Y-VAL-P1HLT/"],title="count mismatch warnings:bona fide bugs but don't report in Savannah per Roger's request",cat=1) #FIXME wrong bug number!!!
@@ -402,6 +402,8 @@ class BugTracker:
         s.add(96760,["'Histogramming-EF-Segment-0-iss' does not exist"])
         s.add(96776,["There was a crash","TrigMinimalEventLoopMgr::sysPrepareForRun"])
         s.add(96354,["Py:AllowedList","xe*","is not in the list of allowed values"])
+        s.add(96858,["There was a crash","/lib64/libc.so.6","TrigSteer_*"])
+        s.add(96876,["OverflowError","bad numeric conversion"])
         return
 
 if __name__ == '__main__':
