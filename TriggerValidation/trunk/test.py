281183d994f2b7b713508cbbaacff3a4f8d0c8ba
kapliy <kapliy@a07cae6d-afd3-2c87-918b-c8c103bdeca4>
2013-05-19 04:24:00 +0000

    fixed TNR for int8r database

--- TriggerValidation/trunk/test.py
+++ TriggerValidation/trunk/test.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
-import cx_Oracle
-ps=''
-connection = cx_Oracle.connect("ATLAS_NICOS_R", ps, "INT8R.CERN.CH")
+
+import sys,cx_Oracle
+assert len(sys.argv)>1
+ps=sys.argv[1]
+connection = cx_Oracle.connect("ATLAS_NICOS_R", ps, "int8r")
