3b5c4a02fb609c99a194410d739df0f0d68eb445
kapliy <kapliy@a07cae6d-afd3-2c87-918b-c8c103bdeca4>
2013-07-25 17:46:36 +0000

    updated experimental code to test accessing new nicos DB from oracle

--- TriggerValidation/trunk/setup_lxplus.sh
+++ TriggerValidation/trunk/setup_lxplus.sh
@@ -3,8 +3,17 @@
 # Sets up a 32-bit athena release
 
 RELEASE=17.0.6.13
+NEED_SETUP=1
+uname -r | grep -q 'el6' && NEED_SETUP=0
 
-export AtlasSetup=/afs/cern.ch/atlas/software/dist/AtlasSetup
-source $AtlasSetup/scripts/asetup.sh ${RELEASE},32,here
-export CLFAGS=-m32
-export LDFLAGS=-m32
+# on SLC5, source a version of athena that comes with Python 2.6
+# (not needed on SLC6)
+if [ "${NEED_SETUP}" == "1" ]; then
+    export AtlasSetup=/afs/cern.ch/atlas/software/dist/AtlasSetup
+    source $AtlasSetup/scripts/asetup.sh ${RELEASE},32,here
+    export CLFAGS=-m32
+    export LDFLAGS=-m32
+fi
+
+# set ORACLE_HOME
+source /afs/cern.ch/project/oracle/script/setoraenv.sh -s prod
--- TriggerValidation/trunk/test.py
+++ TriggerValidation/trunk/test.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python
 
-import sys,cx_Oracle
-#assert len(sys.argv)>1
-ps='nghtldb_r'
-connection = cx_Oracle.connect("ATLAS_NICOS_R", ps, "int8r")
+import sys
+
+try:
+    import cx_Oracle
+except ImportError:
+    print 'ERROR: cannot import cx_Oracle module'
+    print '       if not on lxplus, you can try compiling it by sourcing oracle/source.sh'
+    sys.exit(1)
+
+ps='nghtldb_ru'
+connection = cx_Oracle.connect("ATLAS_NICOS_RUSER", ps, "ATLR")
 cursor = connection.cursor()
-cursor.execute('ALTER SESSION SET current_schema = ATLAS_NICOS_RND')
+cursor.execute('ALTER SESSION SET current_schema = ATLAS_NICOS')
 
 def describe_table():
     cmd='select * from tags where 1=0'
     print cursor.description
+
+PROJECTS = ['AtlasP1HLT','AtlasAnalysis','AtlasTrigger','AtlasHLT','AtlasProduction']
+nname = '17.2.X-VAL' #'17.1.X.Y-VAL2-P1HLT'
+nid,relid,jid = 10, 5312, 201307220010002
 
 cmd="""select res,projname,nameln,contname,to_char(tstamp, 'RR/MM/DD') as tstamp,mngrs from compresults natural join projects natural join packages where jid=201305080008001 and projname='AtlasReconstruction'"""
 
@@ -19,11 +30,30 @@ cmd = "select projname from projects where projname like '%Trig%'"
 cmd = "select N.nname,R.name,P.projname,T.tagname from tags T inner join projects P on P.projid = T.projid inner join releases R on R.relid=T.relid inner join nighlies N on N.nid=T.nid where R.name='rel_2' and rownum<10"
 cmd='select pname,contname from packages where rownum<10'
 cmd = "select N.nname,R.name,P.projname,T.tagname,PK.pname,PK.contname from tags T inner join projects P on P.projid = T.projid inner join releases R on R.relid=T.relid inner join nightlies N on N.nid=T.nid inner join packages PK on PK.pid=T.pid where R.name='rel_2' and N.nname='17.1.X.Y-VAL-P1HLT' and rownum<50"
+cmd=""" select name,tname,fname,type from tests where tests.type='INT' and rownum<20 """
 
-cmd='select name,tname,fname from tests where rownum<10'
+# select nightly
+cmd = """select nname,ngroup,ntype,val from NIGHTLIES where nname='%s'  """%nname
+# select release
+cmd = """select R.nid,R.relid,N.nname,R.name,R.TCREL,R.RELTSTAMP from NIGHTLIES N inner join releases R on N.nid = R.nid where nname='%s' and R.RELTSTAMP > sysdate - interval '6' day order by R.RELTSTAMP"""%nname
+# select jobid (for a given nightly+release, contains various architecture+gcc tags)
+cmd = """ select * from JOBS where nid=%d and relid=%d and OPT='opt' and ARCH='x86_64' """%(nid,relid)
+# jobstat for a jobid (repeated for each project) - USELESS
+cmd = """ select JS.projid,P.projname from JOBSTAT JS inner join projects P on P.projid=JS.projid where JS.jid=%d """%jid
+# compilation statistics for a given project - USELESS
+cmd = """ select P.PROJNAME,C.NPACK,C.NPB,C.NER from CSTAT C inner join PROJECTS P on P.projid=C.projid where C.jid=%d and P.PROJNAME='AtlasAnalysis' """%(jid)
+# compilation results: list of packages with a warning or error
+cmd = """ select P.PROJNAME,PK.PNAME,PK.CONTNAME,CR.Res,CR.ERRTEXT,CR.NAMELN from compresults CR inner join PROJECTS P on P.projid=CR.projid inner join PACKAGES PK on PK.pid=CR.PID where CR.jid=%d and P.PROJNAME='AtlasAnalysis' and CR.Res>0 """%(jid)
+# test results TODO
+cmd = """ select P.PROJNAME,PK.PNAME,PK.CONTNAME,TR.Ecode,TR.CODE,TR.RES,TR.NAME,TR.NAMELN,TR.WDIRLN from testresults TR inner join PROJECTS P on P.projid=TR.projid inner join PACKAGES PK on PK.pid=TR.PID where TR.jid=%d and P.PROJNAME='AtlasAnalysis' and TR.Res>-10 """%(jid)
+
+
 print cmd
 cursor.execute(cmd)
+ntot=0
 for bla in cursor.fetchall():
     print bla
+    ntot+=1
+print 'Total =',ntot
 
 connection.close()
